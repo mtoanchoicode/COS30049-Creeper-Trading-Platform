97558f8b8a14f5c322c0d928f52e152358cea611
Trung <104772921@student.swin.edu.au>
2025-01-10 10:39:11 +0700

    Updated

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-left"></div>
+      <div className="footer-center"></div>
+      <div className="footer-right"></div>
+    </div>
+  );
+};
--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import "./NavBar.css";
+import './NavBar.css';
 
 export const NavBar = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-left"></div>
+      <div className="navbar-center"></div>
+      <div className="navbar-rght"></div>
+    </div>
+  );
 };
