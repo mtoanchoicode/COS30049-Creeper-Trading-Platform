66ebd2ed69fb1ef5c647a4f77bf1937f816af69f
Toan Truong <toantr.work@gmail.com>
2025-01-11 09:45:55 +0700

    Fix Route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,38 @@ import { createRoot } from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/index.css";
+import Dashboard from "./pages/Dashboard.jsx";
+import Market from "./pages/Market.jsx";
+import AboutUs from "./pages/Market.jsx";
+
 import App from "./App.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "market",
+        element: <Market />,
+      },
+      {
+        path: "trading",
+        element: <Trading />,
+      },
+      {
+        path: "about",
+        element: <AboutUs />,
+      },
+    ],
+  },
+  {
+    path: "register",
+    element: <RegisterPage />,
   },
 ]);
 
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>Dasboard</div>;
+};
+
+export default Dashboard;
