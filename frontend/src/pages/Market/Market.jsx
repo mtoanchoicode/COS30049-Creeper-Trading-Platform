11d729af077640d8d2dfaaf0094620005688a670
PGKhang675 <phunggiakhang675@gmail.com>
2025-01-22 23:30:54 +0700

    Coin brief component added

--- frontend/src/pages/Market/Market.jsx
+++ frontend/src/pages/Market/Market.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import MarketTitle from "../../components/MarketTitle/MarketTitle";
-import './Market.css'
+import MarketCoinBrief from "../../components/MarketCoins/MarketCoinBrief/MarketCoinBrief";
+import coinData from '../../data/coins.json';
+import './Market.css';
 
 const MarketPage = () => {
+  const bitcoin = coinData.slice(0,3);
   return (
     <div className="market-page">
       <MarketTitle></MarketTitle> 
+      {bitcoin.map(coin =>(
+        <MarketCoinBrief
+          id={coin.id}
+          name={coin.name}
+          symbol={coin.symbol}
+          current_price={coin.current_price}
+          image={coin.image}
+        />
+      ))
+      }
     </div>
     
   );
