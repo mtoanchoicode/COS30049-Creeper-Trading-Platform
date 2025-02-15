355889b9f269fe7c2f6ce0c1a5a134d7ca7fdff9
Velleigr <phatdinh171205@gmail.com>
2025-02-15 21:21:46 +0700

    Update sort

--- frontend/src/components/News-Page/NewsBlock/NewsBlock.jsx
+++ frontend/src/components/News-Page/NewsBlock/NewsBlock.jsx
@@ -7,6 +7,9 @@ const NewsBlock = () => {
     // State to track the selected category
     const [selectedCategory, setSelectedCategory] = useState("All");
 
+    // State for sorting order (latest or oldest)
+    const [sortOrder, setSortOrder] = useState("latest");
+
     // Categories list (to be displayed as filter options)
     const categories = ["All", "Ethereum", "BNB", "PEPE", "Shiba"];
 
@@ -15,6 +18,15 @@ const NewsBlock = () => {
     const filteredNews = selectedCategory === "All" 
         ? newsData
         : newsData.filter((news) => news.Category === selectedCategory);
+
+    // Sort news by date (latest or oldest)
+    filteredNews.sort((a, b) => {
+        //old
+        const dateA = new Date(a.Date);
+        //new
+        const dateB = new Date(b.Date);
+        return sortOrder === "latest" ? dateB - dateA : dateA - dateB;
+    });
 
 
     return (
@@ -34,6 +46,23 @@ const NewsBlock = () => {
                 ))}
 
             </div>
+
+            <div className="Sort_Filter_Container">
+                <div className="DataOrder_Sort_Container">
+                    <label className="DataOrder_Sort_Label" htmlFor="sortOrder_label">Sort by Date:</label>
+                    <select 
+                        title="DataOrder Sort Select"
+                        className="DataOrder_Sort_Select"
+                        id="sortOrder" 
+                        value={sortOrder} 
+                        onChange={(e) => setSortOrder(e.target.value)}
+                    >
+                        <option className="DataOrder_Sort_Option" value="latest">Latest</option>
+                        <option className="DataOrder_Sort_Option" value="oldest">Oldest</option>
+                    </select>
+                </div>
+            </div>
+           
         
             <div className="NewsBlock_Container">
                 {filteredNews.map((news) => (
