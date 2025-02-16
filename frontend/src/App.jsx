445476444266c45ea0241686ea11e0387fe21a7c
mtoanchoicode <toantr.work@gmail.com>
2025-02-16 11:19:22 +0700

    Chatbot widget

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import "./config/appKitConfig";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import ChatbotWidget from "./components/ChatbotWidget/ChatbotWidget";
 
 function App() {
   const [theme, setTheme] = useState("dark");
@@ -11,20 +12,23 @@ function App() {
 
   useEffect(() => {
     // Set the background color based on the theme
-    document.documentElement.style.backgroundColor = theme === "dark" ? "#131313" : "white";
+    document.documentElement.style.backgroundColor =
+      theme === "dark" ? "#131313" : "white";
     document.documentElement.style.color = theme === "dark" ? "white" : "black";
   }, [theme]);
-
 
   const locationCheck = () => {
     return !location.pathname.startsWith("/trade");
   };
 
   return (
-    <div className={`app ${theme}`}> {/* Use template literal for className */}
+    <div className={`app ${theme}`}>
+      {" "}
+      {/* Use template literal for className */}
       <NavBar theme={theme} setTheme={setTheme} />
       <Outlet />
       {locationCheck() && <Footer />}
+      <ChatbotWidget />
     </div>
   );
 }
