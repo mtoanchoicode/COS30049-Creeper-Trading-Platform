67ed12422718b9d02e028e3a23dbe88c1146af25
Trung <104772921@student.swin.edu.au>
2025-03-14 23:04:59 +0700

    Create Token Component

---
+++ frontend/src/components/Trade/CreateTokenContainer/CreateTokenContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateTokenContainer = () => {
+  return (
+    <div className='create-token-container'>CreateTokenContainer</div>
+  )
+}
+
+export default CreateTokenContainer;
--- frontend/src/pages/Trade/CreateToken.jsx
+++ frontend/src/pages/Trade/CreateToken.jsx
@@ -1,9 +1,51 @@
 import React from "react";
 
+import { ExportOutlined } from "@ant-design/icons";
+import { useAppKitAccount, useAppKit } from "@reown/appkit/react";
+import { Button } from "antd";
+import CreateTokenContainer from "../../components/Trade/CreateTokenContainer/CreateTokenContainer";
+
 const CreateToken = () => {
+  const { isConnected } = useAppKitAccount();
+  const { open } = useAppKit();
+
+  const handleButtonClick = () => {
+    if (!isConnected) {
+      open();
+    } else if (amount) {
+      console.log("Button clicked!");
+    }
+  };
+
+  const getButtonText = () => {
+    if (!isConnected) return "Connect wallet";
+    return "Continue";
+  };
   return (
     <div className="create trade-child">
-      <div className="trade-main"></div>
+      <div className="trade-main">
+        <CreateTokenContainer />
+        <Button
+          type="primary"
+          block
+          className={`swap-btn trade-btn ${
+            isConnected && !swapFromCurrencyValue && !swapToCurrencyValue
+              ? "disabled"
+              : "enabled"
+          }`}
+          onClick={() => handleButtonClick()}
+        >
+          {getButtonText()}
+        </Button>
+        <a
+          className="trade-contractAddress"
+          href={`https://sepolia.etherscan.io/address/}`}
+          target="_blank"
+        >
+          <span>Contract address</span>
+          <ExportOutlined />
+        </a>
+      </div>
     </div>
   );
 };
