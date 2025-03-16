373a731e4bd3a7821e92259fce8c1006a57e4684
Velleigr <phatdinh171205@gmail.com>
2025-03-16 17:10:11 +0700

    fix error of backend models

--- backend/src/models/user_transaction.model.js
+++ backend/src/models/user_transaction.model.js
@@ -1,32 +1,43 @@
+const { getUserByWallet } = require("../models/user_address.model.js")
+const { getTokenIdFromAddress } = require("../models/token.model.js")
 const CreeperDB = require("../config/CreaperDB.js");
 
 const createTransaction = async (userID, tokenID, addressFrom, addressTo, amount, fee, gas, method, hashCode, status) => {
-    const query = `
-        INSERT INTO Transactions (UserID, TokenID, AddressFrom, AddressTo, Amount, Fee, Gas, Method, CreatedAt, HashCode, Status) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW(), $9, $10) 
+
+    try {
+        const IdFromWallet = await getUserByWallet(addressFrom)
+        const uID = IdFromWallet.UserID
+
+        const IdFromToken = await getTokenIdFromAddress(tokenID)
+        const tID = IdFromToken.TokenID
+
+        const  rows  = await CreeperDB.sql`
+        INSERT INTO "Transactions" 
+        ("UserID", "TokenID", "AddressFrom", "AddressTo", "Amount", "Fee", "Gas", "Method", "CreatedAt", "HashCode", "Status") 
+        VALUES (${uID}, ${tID}, ${addressFrom}::TEXT, ${addressTo}::TEXT, ${amount}, ${fee}, ${gas}, ${method}::TEXT, NOW(), ${hashCode}::TEXT, ${status}::TEXT)
         RETURNING *;
     `;
-    try {
-        const { rows } = await CreeperDB.query(query, [userID, tokenID, addressFrom, addressTo, amount, fee, gas, method, hashCode, status]);
         return rows[0];
     } catch (error) {
         console.error('Error creating transaction:', error);
-
     }
 };
 
 const getTransaction = async (hashCode) => {
-    const query = `
-        SELECT * FROM Transactions 
-        WHERE hashCode = $1;
-    `;
 
     try {
-        const { rows } = await CreeperDB.query(query, [hashCode]);
+        const rows = await CreeperDB.sql`
+        SELECT * FROM "Transactions" 
+        WHERE "HashCode" = ${hashCode};
+    `;
         return rows[0];
     } catch (error) {
         console.error('Error getting transaction:', error);
     }
 };
+
+
+
+
 
 module.exports = { createTransaction, getTransaction };
