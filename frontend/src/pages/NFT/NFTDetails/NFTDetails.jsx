29aad92a4b8bf1e5deb5e5bfd72531c121f07167
mtoanchoicode <toantr.work@gmail.com>
2025-03-30 23:36:31 +0700

    Fix error

--- frontend/src/pages/NFT/NFTDetails/NFTDetails.jsx
+++ frontend/src/pages/NFT/NFTDetails/NFTDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import "./NFTDetails.css";
 
